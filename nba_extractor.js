501ead48df5ffed1d513edf8c2c8e227d2345989
Mirza@air <mirzasikander@outlook.com>
2014-04-25 00:48:41 -0700

    Extract script should be generic enough now. project.xml is for fril.

--- nba_extractor.js
+++ nba_extractor.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
-var scraped_data_dir = './scraped-data';
-var extracted_data_dir = './extracted-data'
+var scraped_data_dir = './scraped-data/nba';
+var extracted_data_dir = './extracted-data/nba'
 fs.readdir(scraped_data_dir, readdir)
 var currentRow;
+var currentFile;
 
 function isConsistent(metrics) {
     for (property in metrics) {
@@ -31,13 +32,13 @@ function parseScore(raw_score, metrics, previous_score) {
         }
         isConsistent(metrics);
     } catch (err) {
-        console.log('\nLine number:' + currentRow + '\nData: ' + raw_score);
+        console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_score);
         throw err;
     }
 
 }
 
-function parseShotMade(raw_txt, metrics) {
+function parseShotDetail(raw_txt, metrics) {
     try {
         if (raw_txt.search(/Free Throw/) != -1) {
             var array = raw_txt.match(/^(.*) Free Throw (Technical)?(?:(\d) of (\d))?/)
@@ -47,19 +48,172 @@ function parseShotMade(raw_txt, metrics) {
             metrics.numfreeshot = array[3] || '';
             metrics.outof = array[4] || '';
         } else {
-            var array = raw_txt.match(/^(.*) (?:(\d+)')? (.*) \(\d+ PTS\)(?: \((.*) \d )?/);
+            var array = raw_txt.match(/^(.*) (?:(\d+)')? (?:(.*) \(\d+ PTS\)(?: \((.*) \d )?|(.*))/);
             metrics.eventtype = 'shot'
             metrics.player = array[1];
             metrics.shotdistance = array[2] || '';
-            metrics.type = array[3];
-            metrics.assist = array[5] || '';
+            metrics.type = array[3] || array[5];
+            metrics.assist = array[4] || '';
         }
         isConsistent(metrics);
     } catch (err) {
-        console.log('\nLine number:' + currentRow + '\nData: ' + raw_txt);
+        console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
         throw err;
     }
 }
+
+function parseShotMade(raw_txt, metrics) {
+    parseShotDetail(raw_txt, metrics);
+    metrics.result = 'made';
+}
+
+function parseOtherEvents(raw_txt, metrics) {
+    if (raw_txt.search(/Jump Ball (.*) vs. (.*)/) != -1) {
+        try {
+            var array = raw_txt.match(/^Jump Ball (.*) vs. (.*):/);
+            metrics.eventtype = 'jumpball';
+            metrics.homejumpball = array[1];
+            metrics.awayjumpball = array[2];
+            isConsistent(metrics);
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    } else if (raw_txt.search(/MISS/) != -1) {
+        var array = raw_txt.match(/^MISS (.*)/);
+        parseShotDetail(array[1], metrics);
+        metrics.result = 'missed';
+    } else if (raw_txt.search(/REBOUND|Rebound/) != -1) {
+        try {
+            var array = raw_txt.match(/(.*) (REBOUND|Rebound)/);
+            metrics.eventtype = 'rebound';
+            if (array[2] == 'REBOUND') {
+                metrics.player = array[1];
+            }
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    } else if (raw_txt.search(/FOUL/) != -1) {
+        try {
+            var array = raw_txt.match(/(.*) (\S+)\.FOUL/);
+            metrics.eventtype = 'foul';
+            metrics.player = array[1];
+            if (array[2] == 'P') {
+                metrics.type = 'Personal';
+            } else if (array[2] == 'S') {
+                metrics.type = 'Shooting';
+            } else if (array[2] == 'T') {
+                metrics.type = 'Technical';
+            } else if (array[2] == 'L.B') {
+                metrics.type = 'Loose ball';
+            } else if (array[2] == 'FLAGRANT') {
+                metrics.type = 'Flagrant';
+            } else if (array[2] == 'AWAY.FROM.PLAY') {
+                metrics.type = 'away from play';
+            } else {
+                throw new Error();
+            }
+            isConsistent(metrics);
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    } else if (raw_txt.search(/Foul \(/) != -1) {
+        try {
+            var array = raw_txt.match(/^(\S+) (?:(.*) Foul|(.*)\.Foul)/);
+            metrics.eventtype = 'foul';
+            metrics.player = array[1];
+            if (typeof array[3] != 'undefined') {
+                if (array[3] == 'OFF') {
+                    metrics.type = 'Offensive';
+                }
+            } else {
+                metrics.type = array[2];
+            }
+            isConsistent(metrics);
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    } else if (raw_txt.search(/Turnover/) != -1) {
+        try {
+            var array = raw_txt.match(/^(.*) (?:(Bad Pass|(?:Out of Bounds|Poss )?Lost Ball|Traveling|Foul|(?:3 Second |Jump Ball )Violation|Offensive Goaltending|Kicked Ball Violation|Step Out of Bounds|Palming) Turnover|Turnover: (.*) \()/);
+            metrics.eventtype = 'turnover';
+            if (typeof array[2] == 'undefined') {
+                metrics.reason = array[3];
+            } else {
+                metrics.player = array[1];
+                metrics.reason = array[2];
+            }
+            isConsistent(metrics);
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    } else if (raw_txt.search(/SUB/) != -1) {
+        try {
+            var array = raw_txt.match(/SUB: (.*) FOR (.*)/);
+            metrics.eventtype = 'substitute';
+            metrics.entered = array[1];
+            metrics.left = array[2];
+            isConsistent(metrics);
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    // } else if (raw_txt.search(/STEAL/) != -1) {
+    //     try {
+    //         var array = raw_txt.match(/(\S+) STEAL/);
+    //         metrics.steal = array[1];
+    //     } catch (err) {
+    //         console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+    //         throw err;
+    //     }
+    // } else if (raw_txt.search(/BLOCK/) != -1) {
+    //     try {
+    //         var array = raw_txt.match(/(\S+) BLOCK/);
+    //         metrics.block = array[1];
+    //     } catch (err) {
+    //         console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+    //         throw err;
+    //     }
+    } else if (raw_txt.search(/Violation/) != -1) {
+        try {
+            var array = raw_txt.match(/(.*) Violation:(.*)/);
+            metrics.eventtype = "violation";
+            metrics.player = array[1];
+            metrics.reason = array[2];
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+    } else if (raw_txt.search(/Ejection/) != -1) {
+        try {
+            var array = raw_txt.match(/(.*) Ejection:(.*)/);
+            metrics.eventtype = "ejection";
+            metrics.player = array[1];
+            metrics.reason = array[2];
+        } catch (err) {
+            console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+            throw err;
+        }
+
+    } else if (raw_txt.search(/Timeout/) != -1) {
+        metrics.eventtype = 'timeout';
+    } else {
+        console.log('\nFile:' + currentFile + '\nLine number:' + currentRow + '\nData: ' + raw_txt);
+        throw new Error();
+    }
+}
+
+// function unfold(rows, result){
+//     if(rows.length == 1){
+//         result.push(rows[0]);
+//     }else{
+//         for(var i = 0; )
+//     }
+// }
 
 function readdir(err, files) {
     if (err) {
@@ -68,98 +222,142 @@ function readdir(err, files) {
     }
     for (var i = 0; i < files.length; i++) {
         var file = scraped_data_dir + '/' + files[i];
-        var errFile = [];
-        fs.readFile(file, readFile);
-    }
-}
-
-function readFile(err, data) {
-    if (err) {
-        errFile.push(file);
-        console.log(err);
-        return;
-    }
-    var lines = data.toString().split("\n");
-    var previous_score = {
-        away: "0",
-        home: "0"
-    };
-    var extracted_data = "a1, a2, a3, a4, a5, h1, h2, h3, h4, h5, quarter, time, team, eventtype, assist, awayjumpball, block, entered, homejumpball, left, numfreeshot, opponent, outof, player, points, possession, reason, result, steal, type, x, y, score, shotdistance \n";
-    for (var j = 1; j < lines.length; j++) {
-        if(lines[j] == ""){
-            console.log("\nSkipped line "+currentRow+"\n\n");
+        if (file.search(/.DS_Store/) != -1) {
             continue;
         }
-        currentRow = j; //for debugging 
-        var line = lines[j];
-        var columns = line.split(",");
-        var metrics = {
-            away_score: '',
-            home_score: '',
-            eventtype: '',
-            assist: '',
-            awayjumpball: '',
-            block: '',
-            entered: '',
-            homejumpball: '',
-            left: '',
-            numfreeshot: '',
-            opponent: '',
-            outof: '',
-            player: '',
-            points: '',
-            possession: '',
-            reason: '',
-            result: '',
-            steal: '',
-            shotdistance: ''
-        };
-        parseScore(columns[1], metrics, previous_score);
-        if (metrics.away_score != '') {
-            parseShotMade(columns[3], metrics)
-        }
+        var errFile = [];
+        (function(file) {
+            var extracted_data = fs.readFile(file, function(err, data) {
+                currentFile = file;
+                if (err) {
+                    errFile.push(file);
+                    console.log(err);
+                    return;
+                }
+                var lines = data.toString().split("\n");
+                var previous_score = {
+                    away: "0",
+                    home: "0"
+                };
+                var temp_storage = {
+                    time: "",
 
-        var extracted_columns = new Array(34);
-        extracted_columns[10] = columns[0]; //quarter
-        extracted_columns[11] = columns[4]; //time;
-        extracted_columns[12] = columns[2]; //team
-        extracted_columns[13] = metrics.eventtype;
-        extracted_columns[14] = metrics.assist;
-        extracted_columns[15] = metrics.awayjumpball;
-        extracted_columns[16] = metrics.block;
-        extracted_columns[17] = metrics.entered;
-        extracted_columns[18] = metrics.homejumpball;
-        extracted_columns[19] = metrics.left;
-        extracted_columns[20] = metrics.numfreeshot;
-        extracted_columns[21] = metrics.opponent;
-        extracted_columns[22] = metrics.outof;
-        extracted_columns[23] = metrics.player;
-        extracted_columns[24] = metrics.points;
-        extracted_columns[25] = metrics.possession;
-        extracted_columns[26] = metrics.reason;
-        extracted_columns[27] = metrics.result;
-        extracted_columns[28] = metrics.steal;
-        extracted_columns[29] = metrics.type;
-        extracted_columns[30] = "";
-        extracted_columns[31] = "";
+                }
+                var extracted_data = "a1, a2, a3, a4, a5, h1, h2, h3, h4, h5, quarter, time, team, eventtype, assist, awayjumpball, block, entered, homejumpball, left, numfreeshot, opponent, outof, player, points, possession, reason, result, steal, type, x, y, score, shotdistance, url \n";
+                //var extracted_data = [];
+                for (var j = 1; j < lines.length; j++) {
+                    if (lines[j] == "") {
+                        console.log("\nSkipped line " + currentRow + "\n\n");
+                        continue;
+                    }
+                    currentRow = j; //for debugging 
+                    var line = lines[j];
+                    var columns = line.split(",");
+                    var metrics = {
+                        away_score: '',
+                        home_score: '',
+                        eventtype: '',
+                        assist: '',
+                        awayjumpball: '',
+                        block: '',
+                        entered: '',
+                        homejumpball: '',
+                        left: '',
+                        numfreeshot: '',
+                        opponent: '',
+                        outof: '',
+                        player: '',
+                        points: '',
+                        possession: '',
+                        reason: '',
+                        result: '',
+                        steal: '',
+                        shotdistance: ''
+                    };
 
-        if (metrics.away_score == '') {
-            if (previous_score.home == '0' && previous_score.away == '0') {
-                extracted_columns[32] = "";
-            } else {
-                extracted_columns[32] = previous_score.home + '-' + previous_score.away;
-            }
-        } else {
-            extracted_columns[32] = metrics.home_score + '-' + metrics.away_score;
-            previous_score.away = metrics.away_score;
-            previous_score.home = metrics.home_score;
-        }
+                    if (columns[4].search(/BLOCK/) != -1 || columns[4].search(/STEAL/) != -1) {
+                        continue;
+                    }
 
-        extracted_columns[31] = metrics.shotdistance;
-        var newline = extracted_columns.join();
-        console.log(newline);
-        extracted_data += newline
-    };
+                    parseScore(columns[2], metrics, previous_score);
+                    if (metrics.away_score != '') {
+                        parseShotMade(columns[4], metrics)
+                    } else {
+                        parseOtherEvents(columns[4], metrics);
+                    }
+                    var extracted_columns = new Array(35);
+                    for (var i = 0; i < 10; i++) {
+                        extracted_columns[i] = "";
+                    }
+                    extracted_columns[10] = columns[1]; //quarter
+                    extracted_columns[11] = columns[5].replace(/(\r\n|\n|\r)/gm, "").replace(/(\d\d?):(\d\d)/, "$1.$2"); //time;
+                    extracted_columns[12] = columns[3]; //team
+                    extracted_columns[13] = metrics.eventtype;
+                    extracted_columns[14] = metrics.assist;
+                    extracted_columns[15] = metrics.awayjumpball;
+                    extracted_columns[16] = metrics.block;
+                    extracted_columns[17] = metrics.entered;
+                    extracted_columns[18] = metrics.homejumpball;
+                    extracted_columns[19] = metrics.left;
+                    extracted_columns[20] = metrics.numfreeshot;
+                    extracted_columns[21] = metrics.opponent;
+                    extracted_columns[22] = metrics.outof;
+                    extracted_columns[23] = metrics.player;
+                    extracted_columns[24] = metrics.points;
+                    extracted_columns[25] = metrics.possession;
+                    extracted_columns[26] = metrics.reason;
+                    extracted_columns[27] = metrics.result;
+                    extracted_columns[28] = metrics.steal;
+                    extracted_columns[29] = metrics.type;
+                    extracted_columns[30] = "";
+                    extracted_columns[31] = "";
 
+                    if (metrics.away_score == '') {
+                        if (previous_score.home == '0' && previous_score.away == '0') {
+                            extracted_columns[32] = "";
+                        } else {
+                            extracted_columns[32] = previous_score.home + '-' + previous_score.away;
+                        }
+                    } else {
+                        extracted_columns[32] = metrics.home_score + '-' + metrics.away_score;
+                        previous_score.away = metrics.away_score;
+                        previous_score.home = metrics.home_score;
+                    }
 
+                    extracted_columns[33] = metrics.shotdistance;
+                    extracted_columns[34] = columns[0];
+                    extracted_columns[35] = "\n";
+                    var newline = extracted_columns.join();
+                    console.log(newline);
+                    extracted_data += newline
+                    //extracted_data.push(extracted_columns);
+                };
+
+                // var rows_w_same_time = [];
+                // var current_time = null;
+                // var current_quarter = null;
+                // var result[];
+                // for (var j = 0; j < extracted_data.length; j++) {
+                //    var row = extracted_data[j]; 
+                //    if (row[10] == current_quarter && row[11] == current_time){
+                //         rows_w_same_time.push(row);
+                //    }else{
+                //         unfold(rows_w_same_time, result);
+                //         rows_w_same_time = [];
+                //         rows_w_same_time.push(row);
+                //         current_quarter = row[10];
+                //         current_time = row[11];
+                //    }
+                // }
+                fs.writeFile(extracted_data_dir + '/' + file.replace(/.*\w\/(.*)-raw.csv/, "$1-ex.csv"), extracted_data, function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("The file was saved!");
+                    }
+                });
+
+            })
+        })(file);
+    }
 }
